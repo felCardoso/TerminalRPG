c0a9aa1354d26f059adeccda0f101200c75bc88e
Felipe Cardoso <102108623+felCardoso@users.noreply.github.com>
2025-10-14 22:01:31 -0300

    Update game.py

--- game.py
+++ game.py
@@ -4,9 +4,9 @@ from src.utils.menu import (
     AdventureMenu,
     CitySelectMenu,
     CityMenu,
+    GuildMenu,
     ShopSelectMenu,
     ShopMenu,
-    DungeonInteractMenu,
     DungeonSelectMenu,
     HouseMenu,
 )
@@ -14,10 +14,11 @@ from src.utils.core import (
     SaveManager,
     DungeonManager,
     C,
-    show_intro,
+    # show_intro,
     clear_screen,
     type_text,
     progress_bar,
+    X,
 )
 from src.entities.player import Player, House
 from time import sleep
@@ -29,29 +30,37 @@ sm = SaveManager()
 class Game:
     def __init__(self):
         self.menu = MainMenu(self)
+        self.slot_file = None
 
     def run(self):
-        show_intro()
-        self.menu.run(menu=True)
+        # show_intro()
+        self.menu.run()
 
     def new_game(self):
+        def validate_name():
+            name = False
+            while not name:
+                name = input(f"{C.BLA}Enter your character's name:\n{C.WHI}> {C.BLA}")
+                if name:
+                    return name
+
         clear_screen()
-        slot = sm.select_slot(new=True)
-        if slot:
+        self.slot_file = sm.select_slot(new=True)
+        if self.slot_file:
             clear_screen()
-            name = self.validate_name()
+            name = validate_name()
             player = Player(name)
-            sm.save(player, slot)
-            self.adventure_menu(player, slot)
+            sm.save(player, self.slot_file)
+            self.adventure_menu(player, self.slot_file)
 
     def continue_game(self):
         clear_screen()
-        slot = sm.select_slot(new=False)
-        if slot:
+        self.slot_file = sm.select_slot(new=False)
+        if self.slot_file:
             try:
-                data = sm.load(slot)
+                data = sm.load(self.slot_file)
                 player = Player.from_dict(data)
-                self.adventure_menu(player, slot)
+                self.adventure_menu(player, self.slot_file)
             except (FileNotFoundError, json.JSONDecodeError) as e:
                 clear_screen()
                 type_text(f"{str(e)}", color=C.RED)
@@ -66,20 +75,19 @@ class Game:
         exit()
 
     def save_game(self, player, slot_file):
-        sm.save(player, slot_file)
-
-    def validate_name(self):
-        name = False
-        while not name:
-            name = input(f"\n{C.BLA}Enter your character's name:\n{C.WHI}> {C.BLA}")
-            if name:
-                return name
+        if self.slot_file:
+            sm.save(player, self.slot_file)
+        else:
+            sm.save(player, slot_file)
 
     # MENUS
 
-    def adventure_menu(self, player, slot_file):
-        menu = AdventureMenu(self, player, slot_file)
-        menu.run(True)
+    def adventure_menu(self, player, slot_file=None):
+        if not slot_file:
+            menu = AdventureMenu(self, player, self.slot_file)
+        else:
+            menu = AdventureMenu(self, player, slot_file)
+        menu.run()
 
     def cityselect_menu(self, player):
         menu = CitySelectMenu(self, player)
@@ -89,9 +97,21 @@ class Game:
         menu = CityMenu(self, player)
         menu.run()
 
+    def guild_menu(self, player):
+        from src.locations.world import Guild
+
+        if player.guild:
+            menu = GuildMenu(self, player)
+            menu.run()
+        else:
+            if player.level >= 5:
+                player.guild = Guild(name="Adventurer's Guild", player=player)
+            else:
+                input(f"{X} {C.RED}You need to be at least level 5 to join the Guild")
+
     def shopselect_menu(self, player):
         menu = ShopSelectMenu(self, player)
-        menu.run(True)
+        menu.run()
 
     def shop_menu(self, shop, player):
         menu = ShopMenu(shop, player)
@@ -121,26 +141,33 @@ class Game:
 
     # OTHER
 
-    def travel(self, player, city_id):
-        if city_id and city_id in CITY:
-            city = CITY[city_id]
-            travel_time = int(min((city.level * 48) - (player.location * 12), 1))
-            clear_screen()
+    def travel(self, player, id, opt):
+        def pass_time(x, travel_time):
             print(
-                f"{C.BLU}You started your journey to {C.CYA}{city.name}{C.BLU}.\nThis will take {int(travel_time / 24)} days."
+                f"{C.BLU}You started your journey to {C.CYA}{x.name}{C.BLU}.\nThis will take {int(travel_time / 24)} days."
             )
-            input(travel_time)
             player.pass_time(travel_time)
-            progress_bar(travel_time)
+            progress_bar(travel_time * 24)
             clear_screen()
-            type_text(f"You arrived at {city.name}!", C.GRE)
-            sleep(0.5)
-            clear_screen()
-            player.location = city_id
-            sleep(1)
-            return
+            type_text(f"You arrived at {x.name}!", C.GRE)
+
+        if opt == "c":
+            if id in CITY:
+                city = CITY[id]
+                travel_time = int(abs((city.level * 2) - player.location))
+                pass_time(city, travel_time)
+                player.location = id
+                sleep(0.5)
+                self.adventure_menu(player, slot_file=self.slot_file)
+        elif opt == "d":
+            if id in DUNGEON:
+                dungeon = DUNGEON[id]
+                travel_time = int(abs((dungeon.level * 2) - player.location))
+                pass_time(dungeon, travel_time)
+                dm = DungeonManager(self, dungeon, player)
+                dm.start()
+                sleep(0.5)
 
 
 if __name__ == "__main__":
-    game = Game()
-    game.run()
+    Game().run()
